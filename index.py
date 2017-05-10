cd30ed7115bb941f85f63042c8f4c6af6b101f04
elinfeldborg <elinfeldborg@msn.com>
2017-05-10 21:34:26 +0200

    Add files via upload

---
+++ index.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage_html():
+   return render_template('homepage.html')
+
+@app.route('/stAnton')
+def stAnton_html():
+   return render_template('stAnton.html')
+
+@app.route('/contact')
+def contact_html():
+   return render_template('contact.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
