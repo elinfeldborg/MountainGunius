9295ecc3d32e7705502fca5f78c670a74334c300
elinrossander <elinnrossander@gmail.com>
2017-05-16 14:49:21 +0200

    kommenterat

--- index.py
+++ index.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 def homepage_html():
    return render_template('homepage.html')
 
+@app.route('/homepage')
+def homepage__html():
+   return render_template('homepage.html')
+
 @app.route('/stAnton')
 def stAnton_html():
    return render_template('stAnton.html')
@@ -19,9 +23,27 @@ def stAnton_html():
 def contact_html():
    return render_template('contact.html')
 
+<<<<<<< HEAD
 @app.route('/test')
 def test():
    conn.execute('select * from test')
+=======
+@app.route('/chamonix')
+def chamonix_html():
+   return render_template('chamonix.html')
+
+@app.route('/Ischgl')
+def Ischgl_html():
+   return render_template('Ischgl.html')
+
+@app.route('/mayrhofen')
+def mayrhofen_html():
+   return render_template('mayrhofen.html')
+>>>>>>> origin/master
 
 if __name__ == '__main__':
    app.run(debug = True)
+
+   '''
+hej
+'''
