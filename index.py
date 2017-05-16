8db4dae0cf92fcadf434ad2665c562e15f060363
elinrossander <elinnrossander@gmail.com>
2017-05-16 10:20:49 +0200

    Kopplat databasen

--- index.py
+++ index.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template
+import sqlite3
+
+conn = sqlite3.connect('db/MountainGenius.db')
+print ("Opened database successfully");
+
 
 app = Flask(__name__)
 
@@ -13,6 +18,10 @@ def stAnton_html():
 @app.route('/contact')
 def contact_html():
    return render_template('contact.html')
+
+@app.route('/test')
+def test():
+   conn.execute('select * from test')
 
 if __name__ == '__main__':
    app.run(debug = True)
