e3f7c9028a6bfb5638597e9b986db33c0117ab95
ag7511 <elinfeldborg@msn.com>
2017-05-16 20:10:33 +0200

    Bilderna fungerar fortfarande ej

--- index.py
+++ index.py
@@ -4,12 +4,19 @@ import sqlite3
 conn = sqlite3.connect('db/MountainGenius.db')
 print ("Opened database successfully");
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index_html():
    return render_template('index.html')
+
+@app.route('/elements')
+def elements_html():
+   return render_template('elements.html')
+
+@app.route('/generic')
+def generic_html():
+   return render_template('generic.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
