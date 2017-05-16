83520c96e374e38c41621549da4babf73980c0f7
ag7511 <elinfeldborg@msn.com>
2017-05-16 16:28:44 +0200

    Tagit bort gammal kod som gjorde att det inte fungerade innan

--- index.py
+++ index.py
@@ -8,38 +8,8 @@ print ("Opened database successfully");
 app = Flask(__name__)
 
 @app.route('/')
-def homepage_html():
-   return render_template('homepage.html')
-
-@app.route('/homepage')
-def homepage__html():
-   return render_template('homepage.html')
-
-@app.route('/stAnton')
-def stAnton_html():
-   return render_template('stAnton.html')
-
-@app.route('/contact')
-def contact_html():
-   return render_template('contact.html')
-
-<<<<<<< HEAD
-@app.route('/test')
-def test():
-   conn.execute('select * from test')
-=======
-@app.route('/chamonix')
-def chamonix_html():
-   return render_template('chamonix.html')
-
-@app.route('/Ischgl')
-def Ischgl_html():
-   return render_template('Ischgl.html')
-
-@app.route('/mayrhofen')
-def mayrhofen_html():
-   return render_template('mayrhofen.html')
->>>>>>> origin/master
+def index_html():
+   return render_template('index.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
