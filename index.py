908bb12441a4daab39e81029a8a88495a774aed9
ag7511 <elinfeldborg@msn.com>
2017-05-17 08:41:46 +0200

    Orter
    Har lagt till fler orter

--- index.py
+++ index.py
@@ -14,9 +14,53 @@ def index_html():
 def elements_html():
    return render_template('elements.html')
 
-@app.route('/generic')
-def generic_html():
-   return render_template('generic.html')
+@app.route('/mayrhofen')
+def mayrhofen_html():
+   return render_template('mayrhofen.html')
+
+@app.route('/stanton')
+def stanton_html():
+   return render_template('stanton.html')
+
+@app.route('/ischgl')
+def ischgl_html():
+   return render_template('ischgl.html')
+
+@app.route('/chamonix')
+def chamonix_html():
+   return render_template('chamonix.html')
+
+@app.route('/alpedhuez')
+def alpedhuez_html():
+   return render_template('alpedhuez.html')
+
+@app.route('/valthorens')
+def valthorens_html():
+   return render_template('valthorens.html')
+
+@app.route('/champoluc')
+def champoluc_html():
+   return render_template('champoluc.html')
+
+@app.route('/sestriere')
+def sestriere_html():
+   return render_template('sestriere.html')
+
+@app.route('/canazei')
+def canazei_html():
+   return render_template('canazei.html')
+
+@app.route('/disentis')
+def disentis_html():
+   return render_template('disentis.html')
+
+@app.route('/davos')
+def davos_html():
+   return render_template('davos.html')
+
+@app.route('/engelberg')
+def engelberg_html():
+   return render_template('engelberg.html')
 
 if __name__ == '__main__':
    app.run(debug = True)
