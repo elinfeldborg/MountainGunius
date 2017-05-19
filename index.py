e8528c9b7c82ca733aae0bec92527a66e2c49b79
ah2333 <ellinor.maveus@hotmail.com>
2017-05-19 14:40:22 +0200

    Flask-mail färdigställd

--- index.py
+++ index.py
@@ -1,10 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
+from flask_mail import Mail, Message
 
 conn = sqlite3.connect('db/MountainGenius.db')
 print ("Opened database successfully");
 
 app = Flask(__name__, static_url_path = "",static_folder= "")
+
+app.config.update(
+	DEBUG=True,
+	MAIL_SERVER='smtp.gmail.com',
+	MAIL_PORT=465,
+	MAIL_USE_SSL=True,
+	MAIL_USERNAME = 'mountaingenius17@gmail.com',
+	MAIL_PASSWORD = 'elinelinelli'
+	)
+mail=Mail(app)
 
 @app.route('/')
 def index_html():
@@ -65,6 +76,17 @@ def davos_html():
 @app.route('/engelberg')
 def engelberg_html():
    return render_template('engelberg.html')
+
+@app.route('/thanks',methods = ["GET", "POST"])
+def thanks():
+   name = request.form["name"],
+   email = request.form["email"]
+   message = request.form["message"]
+   text = "Användare: {}\nE-post: {}\nMeddelande: {}".format(name, email, message)
+   msg = Message('Mountain Genius', sender='mountaingenius17@gmail.com', recipients= ['mountaingenius17@gmail.com'])
+   msg.body = text
+   mail.send(msg)
+   return render_template("index.html")
 
 if __name__ == '__main__':
    app.run(debug = True)
